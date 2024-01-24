1f092e98e62c098a7062bb7506e7ac4d0ff5e2db
khaled <khaledNoori@gmail.com>
2024-01-24 10:37:06 +0430

    59-shopping-component@PakageCom

---
+++ src/containers/Shpping/Shopping.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Wrapper from '../../hoc/Wrapper';
+
+class Shopping extends React.Component{
+    render(){
+
+        return(
+            <Wrapper>
+                <fiv>Controls</fiv>
+            </Wrapper>
+        )
+    }
+}
+
+export default Shopping
